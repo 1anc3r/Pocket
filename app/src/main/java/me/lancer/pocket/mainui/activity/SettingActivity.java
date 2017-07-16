b6e560869915d66fb838652ab44fff0864183784
HuangFangzhi <huangfangzhi@icloud.com>
2017-07-16 14:19:11 +0800

    Update log..

--- app/src/main/java/me/lancer/pocket/mainui/activity/SettingActivity.java
+++ app/src/main/java/me/lancer/pocket/mainui/activity/SettingActivity.java
@@ -54,7 +54,7 @@ public class SettingActivity extends BaseActivity {
     private App app;
 
     private Toolbar toolbar;
-    private LinearLayout llNight, llBright, llTheme, llFunc, llProblem, llFeedback, llDownload, llAboutUs;
+    private LinearLayout llNight, llBright, llTheme, llFunc, llProblem, llUpdate, llFeedback, llDownload, llAboutUs;
     private Button btnLoginOut;
     private SwitchCompat scNight;
     private BottomSheetDialog listDialog;
@@ -75,7 +75,37 @@ public class SettingActivity extends BaseActivity {
     private Runnable repository = new Runnable() {
         @Override
         public void run() {
-            String content = contentGetterSetter.getContentFromHtml("repository", "https://raw.githubusercontent.com/1anc3r/1anc3r-s-Programming-Journey/master/AppLink.md");
+            String content = contentGetterSetter.getContentFromHtml("repository", "raw.githubusercontent.com/1anc3r/1anc3r-s-Programming-Journey/master/AppLink.md");
+            if (!content.contains("获取失败!")) {
+                try {
+                    List<RepositoryBean> list = new ArrayList<>();
+                    JSONObject jsonObj = new JSONObject(content);
+                    JSONArray jsonArr = jsonObj.getJSONArray("apps");
+                    for (int i = 0; i < jsonArr.length(); i++) {
+                        RepositoryBean bean = new RepositoryBean();
+                        JSONObject jsonItem = jsonArr.getJSONObject(i);
+                        bean.setImg(jsonItem.getString("img"));
+                        bean.setName(jsonItem.getString("name"));
+                        bean.setDescription(jsonItem.getString("description"));
+                        bean.setDownload(jsonItem.getString("download"));
+                        bean.setBlog(jsonItem.getString("blog"));
+                        list.add(bean);
+                    }
+                    Message msg = new Message();
+                    msg.what = 0;
+                    msg.obj = list;
+                    handler.sendMessage(msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    private Runnable update = new Runnable() {
+        @Override
+        public void run() {
+            String content = contentGetterSetter.getContentFromHtml("repository", "updates.json");
             if (!content.contains("获取失败!")) {
                 try {
                     List<RepositoryBean> list = new ArrayList<>();
@@ -138,6 +168,8 @@ public class SettingActivity extends BaseActivity {
         llFunc.setOnClickListener(vOnClickListener);
         llProblem = (LinearLayout) findViewById(R.id.ll_problem);
         llProblem.setOnClickListener(vOnClickListener);
+        llUpdate = (LinearLayout) findViewById(R.id.ll_update);
+        llUpdate.setOnClickListener(vOnClickListener);
         llFeedback = (LinearLayout) findViewById(R.id.ll_feedback);
         llFeedback.setOnClickListener(vOnClickListener);
         llDownload = (LinearLayout) findViewById(R.id.ll_download);
@@ -160,89 +192,135 @@ public class SettingActivity extends BaseActivity {
         scNight.setClickable(false);
         funcList.add("— 工具 —");
         funcList.add("电话、通讯录、信息 : \n" +
-                "\t\t\t\t电话 : 您手机上的通话记录\n" +
-                "\t\t\t\t通讯录 : 您手机上的联系人\n" +
-                "\t\t\t\t信息 : 您手机上的短信记录");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 电话 : 列出手机上的通话记录\n" +
+                "\t\t\t\t * 通讯录 : 列出手机上的联系人\n" +
+                "\t\t\t\t * 信息 : 列出手机上的短信记录\n" +
+                "\t\t\t\t */");
         funcList.add("图片、音乐、视频、文档、应用、存储 : \n" +
-                "\t\t\t\t您手机上的各种类型的文件, 支持打开、删除、赋值、剪切");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 列出手机上的各种类型的文件, 支持打开、删除、赋值、剪切\n" +
+                "\t\t\t\t */");
         funcList.add("日历 : \n" +
-                "\t\t\t\t一个普通的日历");
-        funcList.add("时钟 : \n" +
-                "\t\t\t\t正在卖力开发中...");
-        funcList.add("备忘录 : \n" +
-                "\t\t\t\t正在卖力开发中...");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 一个普通的日历\n" +
+                "\t\t\t\t * 需要手动添加事项\n" +
+                "\t\t\t\t */");
+//        funcList.add("时钟 : \n" +
+//                "\t\t\t\t正在卖力开发中...");
+//        funcList.add("备忘录 : \n" +
+//                "\t\t\t\t正在卖力开发中...");
         funcList.add("天气 : \n" +
-                "\t\t\t\t天气信息 : 提供全国各城市的天气信息\n" +
-                "\t\t\t\t城市选择 : 通过列表点选或搜索名称的方式选择城市\n" +
-                "\t\t\t\t — 数据来源 : 中央天气\n\t\t\t\t（http://tj.nineton.cn/Heart/index）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 天气信息 : 提供全国各城市的天气信息\n" +
+                "\t\t\t\t * 城市选择 : 通过列表点选或搜索名称的方式选择城市\n" +
+                "\t\t\t\t * ——数据来源 : 中央天气\n\t\t\t\t（tj.nineton.cn/Heart/index）\n" +
+                "\t\t\t\t */");
         funcList.add("翻译 : \n" +
-                "\t\t\t\t — 翻译支持 : 必应词典\n\t\t\t\t（http://cn.bing.com/dict/）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * ——翻译支持 : 必应词典\n\t\t\t\t（cn.bing.com/dict/）\n" +
+                "\t\t\t\t */");
         funcList.add("摩斯电码 : \n" +
-                "\t\t\t\t在字符串和摩斯电码之间任意转换\n"+
-                "\t\t\t\t长按粘贴, 双击复制\n");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 在字符串和摩斯电码之间任意转换\n"+
+                "\t\t\t\t * 长按粘贴, 双击复制\n" +
+                "\t\t\t\t */");
         funcList.add("计算器 : \n" +
-                "\t\t\t\t这是一个支持多项式运算的计算器, 由栈实现");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 这是一个支持多项式运算的计算器, 由栈实现\n" +
+                "\t\t\t\t */");
         funcList.add("二维码 : \n" +
-                "\t\t\t\t扫描 : 调用后置摄像头扫描二维码\n" +
-                "\t\t\t\t识别 : 识别本机图片中的二维码\n" +
-                "\t\t\t\t — 使用开源库 : （https://github.com/scola/Qart）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 扫描 : 调用后置摄像头扫描二维码\n" +
+                "\t\t\t\t * 识别 : 识别本机图片中的二维码\n" +
+                "\t\t\t\t * ——使用开源库 : （github.com/scola/Qart）\n" +
+                "\t\t\t\t */");
         funcList.add("— 资讯 —");
         funcList.add("文章 : \n" +
-                "\t\t\t\t每日一文 : 每天一篇精选优质短篇\n" +
-                "\t\t\t\t随机文章 : 点击刷新按钮随机读文章\n" +
-                "\t\t\t\t — 数据来源 : " +
-                "\n\t\t\t\t 每日一文\n\t\t\t\t（https://meiriyiwen.com）" +
-                "\n\t\t\t\t 背景图片\n\t\t\t\t（https://bing.ioliu.cn）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 每日一文 : 每天一篇精选优质短篇\n" +
+                "\t\t\t\t * 随机文章 : 点击刷新按钮随机读文章\n" +
+                "\t\t\t\t * ——数据来源 : " +
+                "\n\t\t\t\t *  每日一文\n\t\t\t\t（meiriyiwen.com）" +
+                "\n\t\t\t\t *  背景图片\n\t\t\t\t（bing.ioliu.cn）\n" +
+                "\t\t\t\t */");
         funcList.add("趣闻 : \n" +
-                "\t\t\t\t每日 : 知乎日报的每日信息\n" +
-                "\t\t\t\t热门 : 知乎日报的热门信息\n" +
-                "\t\t\t\t分类 : 包括动漫、游戏、财经、电影、音乐、互联网安全等日报\n" +
-                "\t\t\t\t — 数据来源 : 知乎日报\n\t\t\t\t（http://news-at.zhihu.com/api）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 每日 : 知乎日报的每日信息\n" +
+                "\t\t\t\t * 热门 : 知乎日报的热门信息\n" +
+                "\t\t\t\t * 分类 : 包括动漫、游戏、财经、电影、音乐、互联网安全等日报\n" +
+                "\t\t\t\t * ——数据来源 : 知乎日报\n\t\t\t\t（news-at.zhihu.com/api）\n" +
+                "\t\t\t\t */");
         funcList.add("段子 : \n" +
-                "\t\t\t\t段子 : 内涵段子的热辣段子\n" +
-                "\t\t\t\t图片 : 内涵段子的爆笑图片\n" +
-                "\t\t\t\t — 数据来源 : 内涵段子\n\t\t\t\t（http://neihanshequ.com）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 段子 : 内涵段子的热辣段子\n" +
+                "\t\t\t\t * 图片 : 内涵段子的爆笑图片\n" +
+                "\t\t\t\t * ——数据来源 : 内涵段子\n\t\t\t\t（neihanshequ.com）\n" +
+                "\t\t\t\t */");
         funcList.add("图书 : \n" +
-                "\t\t\t\t书评 : 豆瓣读书的最受欢迎书评\n" +
-                "\t\t\t\t书榜 : 爬取呈现豆瓣图书TOP250\n" +
-                "\t\t\t\t搜索 : 点击右上角的搜索按钮搜索你想了解的图书信息\n" +
-                "\t\t\t\t — 数据来源 : 豆瓣读书\n\t\t\t\t（https://book.douban.com）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 书评 : 豆瓣读书的最受欢迎书评\n" +
+                "\t\t\t\t * 书榜 : 爬取呈现豆瓣图书TOP250\n" +
+                "\t\t\t\t * 搜索 : 点击右上角的搜索按钮搜索你想了解的图书信息\n" +
+                "\t\t\t\t * ——数据来源 : 豆瓣读书\n\t\t\t\t（book.douban.com）\n" +
+                "\t\t\t\t */");
         funcList.add("音乐 : \n" +
-                "\t\t\t\t乐评 : 豆瓣音乐的最受欢迎乐评\n" +
-                "\t\t\t\t乐榜 : 爬取呈现豆瓣音乐TOP250\n" +
-                "\t\t\t\t搜索 : 点击右上角的搜索按钮搜索你想了解的音乐信息\n" +
-                "\t\t\t\t — 数据来源 : 豆瓣音乐\n\t\t\t\t（https://music.douban.com）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 乐评 : 豆瓣音乐的最受欢迎乐评\n" +
+                "\t\t\t\t * 乐榜 : 爬取呈现豆瓣音乐TOP250\n" +
+                "\t\t\t\t * 搜索 : 点击右上角的搜索按钮搜索你想了解的音乐信息\n" +
+                "\t\t\t\t * ——数据来源 : 豆瓣音乐\n\t\t\t\t（music.douban.com）\n" +
+                "\t\t\t\t */");
         funcList.add("电影 : \n" +
-                "\t\t\t\t影评 : 豆瓣电影的最受欢迎影评\n" +
-                "\t\t\t\t影榜 : 爬取呈现豆瓣电影TOP250\n" +
-                "\t\t\t\t搜索 : 点击右上角的搜索按钮搜索你想了解的电影信息\n" +
-                "\t\t\t\t — 数据来源 : 豆瓣电影\n\t\t\t\t（https://movie.douban.com）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 影评 : 豆瓣电影的最受欢迎影评\n" +
+                "\t\t\t\t * 影榜 : 爬取呈现豆瓣电影TOP250\n" +
+                "\t\t\t\t * 搜索 : 点击右上角的搜索按钮搜索你想了解的电影信息\n" +
+                "\t\t\t\t * ——数据来源 : 豆瓣电影\n\t\t\t\t（movie.douban.com）\n" +
+                "\t\t\t\t */");
         funcList.add("图片 : \n" +
-                "\t\t\t\t妹子 : 好看的妹子图\n" +
-                "\t\t\t\t美景 : 好看的风景照\n" +
-                "\t\t\t\t — 数据来源 : " +
-                "\n\t\t\t\t 佳人 : Gank.io\n\t\t\t\t（http://gank.io）" +
-                "\n\t\t\t\t 美图 : Pexels Popular Photos\n\t\t\t\t（https://www.pexels.com）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 妹子 : 好看的妹子图\n" +
+                "\t\t\t\t * 美景 : 好看的风景照\n" +
+                "\t\t\t\t * ——数据来源 : " +
+                "\n\t\t\t\t *  佳人 : Gank.io\n\t\t\t\t（gank.io）" +
+                "\n\t\t\t\t *  美图 : Pexels Popular Photos\n\t\t\t\t（www.pexels.com）\n" +
+                "\t\t\t\t */");
+        funcList.add("小说 : \n" +
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 排行 : 小说排行榜\n" +
+                "\t\t\t\t * 分类 : 小说各分区\n" +
+                "\t\t\t\t * 搜索 : 点击右上角的搜索按钮搜索你想看的小说\n" +
+                "\t\t\t\t * ——数据来源 : 追书神器\n\t\t\t\t（https://www.zhuishushenqi.com）\n" +
+                "\t\t\t\t */");
         funcList.add("漫画 : \n" +
-                "\t\t\t\t推荐 : 推荐好看的漫画\n" +
-                "\t\t\t\t排行 : 漫画排行榜\n" +
-                "\t\t\t\t分类 : 来自有妖气各分区排行榜\n" +
-                "\t\t\t\t搜索 : 点击右上角的搜索按钮搜索你想看的漫画\n" +
-                "\t\t\t\t — 数据来源 : 有妖气\n\t\t\t\t（https://www.u17.com）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 推荐 : 推荐好看的漫画\n" +
+                "\t\t\t\t * 排行 : 漫画排行榜\n" +
+                "\t\t\t\t * 分类 : 来自有妖气各分区排行榜\n" +
+                "\t\t\t\t * 搜索 : 点击右上角的搜索按钮搜索你想看的漫画\n" +
+                "\t\t\t\t * ——数据来源 : 有妖气\n\t\t\t\t（www.u17.com）\n" +
+                "\t\t\t\t */");
         funcList.add("视频 : \n" +
-                "\t\t\t\t分类 : 来自B站各分区排行榜前十\n" +
-                "\t\t\t\t — 数据来源 : BiliBili\n\t\t\t\t（http://api.bilibili.com）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 分类 : 来自B站各分区排行榜前十\n" +
+                "\t\t\t\t * ——数据来源 : BiliBili\n\t\t\t\t（api.bilibili.com）\n" +
+                "\t\t\t\t */");
         funcList.add("游戏 : \n" +
-                "\t\t\t\t精选 : 精选各大平台热门游戏\n" +
-                "\t\t\t\t优惠、热销、新品、即将推出\n" +
-                "\t\t\t\t — 数据来源 : Steam\n\t\t\t\t（https://store.steampowered.com）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 精选 : 精选各大平台热门游戏\n" +
+                "\t\t\t\t * 优惠、热销、新品、即将推出\n" +
+                "\t\t\t\t * ——数据来源 : Steam\n\t\t\t\t（store.steampowered.com）\n" +
+                "\t\t\t\t */");
         funcList.add("编程 : \n" +
-                "\t\t\t\t个人 : GitHub上Star最多的个人\n" +
-                "\t\t\t\t组织 : GitHub上Star最多的组织\n" +
-                "\t\t\t\t项目 : GitHub上Star最多的项目\n" +
-                "\t\t\t\t趋势 : GitHub上今日最热的项目\n" +
-                "\t\t\t\t搜索 : 点击右上角的搜索按钮搜索你想浏览的项目\n" +
-                "\t\t\t\t — 数据来源 : GithubRanking\n\t\t\t\t（https://github-ranking.com）");
+                "\t\t\t\t/*\n"  +
+                "\t\t\t\t * 个人 : GitHub上Star最多的个人\n" +
+                "\t\t\t\t * 组织 : GitHub上Star最多的组织\n" +
+                "\t\t\t\t * 项目 : GitHub上Star最多的项目\n" +
+                "\t\t\t\t * 趋势 : GitHub上今日最热的项目\n" +
+                "\t\t\t\t * 搜索 : 点击右上角的搜索按钮搜索你想浏览的项目\n" +
+                "\t\t\t\t * ——数据来源 : GithubRanking\n\t\t\t\t（github-ranking.com）\n" +
+                "\t\t\t\t */");
         problemList.add("Q : 主页上的搜索栏好像并没有什么用...");
         problemList.add("A : 因为模块比较多, 各模块的搜索接口还没有统一, 个别模块中有搜索功能, 下一个版本搜索功能会上线. 如果您对搜索功能有什么好的建议或意见请通过意见反馈通道或者发送邮件联系1anc3r. ");
         problemList.add("Q : 为什么 \"信息\" 列表不显示联系人的名字?");
@@ -365,7 +443,9 @@ public class SettingActivity extends BaseActivity {
                 showListDialog(1, funcList);
             } else if (v == llProblem) {
                 showListDialog(2, problemList);
-            } else if (v == llFeedback) {
+            } else if (v == llUpdate) {
+                new Thread(update).start();
+            }  else if (v == llFeedback) {
                 Instabug.invoke(InstabugInvocationMode.NEW_CHAT);
             } else if (v == llDownload) {
                 new Thread(repository).start();
@@ -413,7 +493,7 @@ public class SettingActivity extends BaseActivity {
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setAction("android.intent.action.VIEW");
-                Uri content_url = Uri.parse("http://www.xiyoumobile.com/");
+                Uri content_url = Uri.parse("https://www.xiyoumobile.com/");
                 intent.setData(content_url);
                 startActivity(intent);
             }
@@ -425,7 +505,7 @@ public class SettingActivity extends BaseActivity {
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setAction("android.intent.action.VIEW");
-                Uri content_url = Uri.parse("http://www.1anc3r.me");
+                Uri content_url = Uri.parse("https://www.1anc3r.me");
                 intent.setData(content_url);
                 startActivity(intent);
             }
@@ -438,7 +518,7 @@ public class SettingActivity extends BaseActivity {
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setType("text/plain");
                 intent.putExtra(Intent.EXTRA_SUBJECT, "分享");
-                intent.putExtra(Intent.EXTRA_TEXT, "看看我发现了什么宝贝(ง •̀_•́)ง\nhttp://www.coolapk.com/apk/me.lancer.pocket");
+                intent.putExtra(Intent.EXTRA_TEXT, "看看我发现了什么宝贝(ง •̀_•́)ง\nhttps://www.coolapk.com/apk/me.lancer.pocket" +"\n分享自口袋");
                 startActivity(Intent.createChooser(intent, "分享到"));
             }
         });
